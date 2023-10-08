97ad81a562026841e23e9be13c5c5e3cabc0a963
chanwooDev <pove2019@gmail.com>
2023-10-09 02:17:25 +0900

    드론 모듈 생성

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import lands, my_drone
 from drone_sitl import DroneSitl 
-
-def seed_start(connection_string):
-    global_location = my_drone.get_global_location(connection_string)
-    angle = lands.find_subomotor_angle(global_location.lat ,global_location.lon)
+from my_drone import MyDrone
 
 drone_sitl = DroneSitl()
-seed_start(drone_sitl.sitl.connection_string())
-
-
+my_drone = MyDrone(drone_sitl.sitl.connection_string())
+my_drone.seed_start()
--- my_drone.py
+++ my_drone.py
@@ -1,39 +1,56 @@
 from dronekit import connect, VehicleMode, Vehicle
 from lands import Land, CoordinateSystem
+from servomotor import Servomotor
 
 NOT_CONATAIN_ANGLE = 0
+OUT_OF_LANDS = -1
 
-def get_global_location(connection_string): #"tcp:127.0.0.1:5760"
+class MyDrone:
 
-    # Connect to the Vehicle.
-    print("Connecting to vehicle on: %s" % (connection_string,))
-    vehicle = connect(connection_string, wait_ready=False)
+    def __init__(self, connection_string):
+        self.lands = self.__init_lands()
+        self.current_land_index = OUT_OF_LANDS
+        self.connection_string = connection_string
+        self.servomotor = Servomotor()
 
-    global_frame = vehicle.location.global_frame
+    def __init_lands(self):
+        lu = CoordinateSystem(0, 2)
+        ru = CoordinateSystem(2, 2)
+        ld = CoordinateSystem(0, 0)
+        rd = CoordinateSystem(2, 0)
+        return [Land(lu, ru, ld, rd, 45)]    
 
-    # Close vehicle object before exiting script
-    vehicle.close()
-    print("Completed")
+    def seed_start(self):
+        while True:
+            new_current_index = self.__find_current_land_index()
 
-    return global_frame
+            if new_current_index != self.current_land_index:
+                print("구역을 이동했으므로 서보모터 각도를 변경합니다")
+                self.current_land_index = new_current_index
+                angle = self.lands[new_current_index].sub_motor_angle if new_current_index != OUT_OF_LANDS else 0
+                self.servomotor.change_angle(angle)
 
-#todo:고도에 따라 닫아줘야함
-def find_subomotor_angle(lat, lon):
-    coordinateSystem = CoordinateSystem(lat, lon)
+    def __get_global_location(self):
 
-    lands = init_lands()
-    for land in lands:
-        if (land.contain(coordinateSystem)):
-            return land.sub_motor_angle
+        # Connect to the Vehicle.
+        print("Connecting to vehicle on: %s" % (self.connection_string,))
+        vehicle = connect(self.connection_string, wait_ready=False)
 
-    return NOT_CONATAIN_ANGLE
+        global_frame = vehicle.location.global_frame
 
-print(find_subomotor_angle(1,1))
+        # Close vehicle object before exiting script
+        vehicle.close()
+        print("location 가져오기 Completed")
 
-def init_lands():
-    lu = CoordinateSystem(0, 2)
-    ru = CoordinateSystem(2, 2)
-    ld = CoordinateSystem(0, 0)
-    rd = CoordinateSystem(2, 0)
-    return [Land(lu, ru, ld, rd, 50)]
+        return global_frame
     
+    def __find_current_land_index(self):
+        global_location = self.__get_global_location()
+        drone_coordinate_system = CoordinateSystem(global_location.lat, global_location.lon)
+
+        for index, land in enumerate(self.lands):
+            if land.contain(drone_coordinate_system):
+                return index
+    
+        return OUT_OF_LANDS    
+        
