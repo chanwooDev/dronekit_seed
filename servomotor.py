f584c175d5a5be9c313847d7ca55c45d98b9653c
chanwooDev <pove2019@gmail.com>
2023-10-09 00:27:20 +0900

    함수명 변경, 예외처리 추가

--- servomotor.py
+++ servomotor.py
@@ -13,14 +13,13 @@ class Servomotor:
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(servo_pin, GPIO.OUT)
         self.pwm =  GPIO.PWM(servo_pin, 50)  # 50Hz (서보모터 PWM 동작을 위한 주파수) - 50hz은 공식적으로 약속된 값임
-        self.pwm.start(3.0) #서보의 0도 위치(0.6ms)이동:값 3.0은 pwm주기인 20ms의 3%를 의미하므로,0.6ms됨.
-
-    def change_duty_cycle(self, number):
-        if duty_cycle_by_angle.get(number) == None:
-            print("없는 값을 입력했습니다. in  change_duty_cycle ")
-            return
+        self.change_angle(0)
         
-        self.pwm.ChangeDutyCycle(duty_cycle_by_angle[number])
+    def change_angle(self, angle):
+        if duty_cycle_by_angle.get(angle) == None:
+            raise Exception("없는 값을 입력했습니다. number: "+ angle)
+        
+        self.pwm.ChangeDutyCycle(duty_cycle_by_angle[angle])
     
     def __del__(self):
         self.pwm.stop()
@@ -29,9 +28,9 @@ class Servomotor:
 
 
 servomoter = Servomotor()
-servomoter.change_duty_cycle(0)
+servomoter.change_angle(0)
 time.sleep(1)
-servomoter.change_duty_cycle(90)
+servomoter.change_angle(90)
 time.sleep(3)
 
 
