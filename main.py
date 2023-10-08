392da877052a6de1a0d1af2b7458618cd8558d65
chanwooDev <pove2019@gmail.com>
2023-10-09 00:54:19 +0900

    모듈명 컨벤션 지켜서 수정

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import lands, mydrone
+import lands, my_drone
 from drone_sitl import DroneSitl 
 
 def seed_start(connection_string):
-    global_location = mydrone.get_global_location(connection_string)
+    global_location = my_drone.get_global_location(connection_string)
     angle = lands.find_subomotor_angle(global_location.lat ,global_location.lon)
 
 drone_sitl = DroneSitl()
