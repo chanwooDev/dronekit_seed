03639b6c6b938fad72696ec1ce0cd906bf71ecb2
chanwooDev <pove2019@gmail.com>
2023-10-10 23:19:23 +0900

    실제, 가상 드론 실행문 추가

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from drone_sitl import DroneSitl 
 from my_drone import MyDrone
 
-drone_sitl = DroneSitl()
-my_drone = MyDrone(drone_sitl.sitl.connection_string())
-my_drone.seed_start()
+
+def sitl_start():
+    drone_sitl = DroneSitl()
+    start(drone_sitl.sitl.connection_string())
+
+def real_start():
+    start("/dev/ttyACM0")
+
+def start(connection_string):
+    my_drone = MyDrone(connection_string)
+    my_drone.seed_start()
+
+real_start()
